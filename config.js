af154c45051e04ff0ed414b3573633cc43a693ad
dz-one1 <vivavoir150@gmail.com>
2025-11-28 22:30:12 +0100

    تحديث إعدادات المتجر - 28/11/2025 22:30:10

--- config.js
+++ config.js
@@ -284,7 +284,7 @@ const STORE_CONFIG = {
   "FREE_DELIVERY": {
     "desk": {
       "enabled": true,
-      "minAmount": 7000,
+      "minAmount": 1,
       "wilayas": [
         "01 - أدرار",
         "02 - الشلف",
@@ -348,7 +348,7 @@ const STORE_CONFIG = {
     },
     "home": {
       "enabled": true,
-      "minAmount": 7000,
+      "minAmount": 1,
       "wilayas": [
         "01 - أدرار",
         "02 - الشلف",
