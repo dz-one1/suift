56292c7f82aad870958aabe3d712755029acfa09
dz-one1 <vivavoir150@gmail.com>
2025-12-02 23:36:41 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,728 @@
+const STORE_CONFIG = {
+  "PRODUCTS": {
+    "2": {
+      "name": "مودال1",
+      "price": 2900,
+      "image": "https://raw.githubusercontent.com/dz-one1/suift/main/images/product-2-1764363735577-Screenshot_20251128-190652_Gallery.jpg",
+      "description": "قمحة تركية",
+      "availableSizes": [
+        "S",
+        "M",
+        "L"
+      ],
+      "availableColors": [
+        "أزرق",
+        "أحمر"
+      ],
+      "ageRanges": [],
+      "colorImages": {
+        "أزرق": {
+          "images": [
+            "https://raw.githubusercontent.com/dz-one1/suift/main/images/product-2-أزرق-1764363830146-Screenshot_20251128-190616_Gallery.jpg"
+          ],
+          "ageRanges": [
+            {
+              "ageFrom": "4",
+              "ageTo": "5",
+              "size": "S"
+            },
+            {
+              "ageFrom": "6",
+              "ageTo": "7",
+              "size": "M"
+            }
+          ]
+        },
+        "أحمر": {
+          "images": [
+            "https://raw.githubusercontent.com/dz-one1/suift/main/images/product-2-أحمر-1764363896583-Screenshot_20251128-190638_Gallery.jpg"
+          ],
+          "ageRanges": [
+            {
+              "ageFrom": "6",
+              "ageTo": "7",
+              "size": "M"
+            }
+          ]
+        }
+      },
+      "sizeGuide": {
+        "enabled": false,
+        "guide": {}
+      }
+    },
+    "3": {
+      "name": "مودال2",
+      "price": 5800,
+      "image": "https://raw.githubusercontent.com/dz-one1/suift/main/images/product-3-1764433655760-Adobe%20Express%20-%20file.jpg",
+      "description": "",
+      "availableSizes": [
+        "S",
+        "M",
+        "XL",
+        "L"
+      ],
+      "availableColors": [
+        "وردي"
+      ],
+      "ageRanges": [
+        {
+          "ageFrom": "5",
+          "ageTo": "6",
+          "size": "S"
+        },
+        {
+          "ageFrom": "7",
+          "ageTo": "8",
+          "size": "M"
+        },
+        {
+          "ageFrom": "9",
+          "ageTo": "10",
+          "size": "L"
+        },
+        {
+          "ageFrom": "11",
+          "ageTo": "12",
+          "size": "XL"
+        }
+      ],
+      "colorImages": {},
+      "sizeGuide": {
+        "enabled": false,
+        "guide": {}
+      }
+    },
+    "4": {
+      "name": "مودال3",
+      "price": 6000,
+      "image": "https://raw.githubusercontent.com/dz-one1/suift/main/images/product-4-1764629908672-modal7.jpg.webp",
+      "description": "",
+      "availableSizes": [
+        "S",
+        "XL",
+        "XXL"
+      ],
+      "availableColors": [
+        "وردي",
+        "أزرق"
+      ],
+      "ageRanges": [],
+      "colorImages": {},
+      "sizeGuide": {
+        "enabled": false,
+        "guide": {}
+      }
+    }
+  },
+  "DELIVERY_PRICES": {
+    "01 - أدرار": {
+      "home": 1100,
+      "desk": 600
+    },
+    "02 - الشلف": {
+      "home": 700,
+      "desk": 400
+    },
+    "03 - الأغواط": {
+      "home": 1000,
+      "desk": 600
+    },
+    "04 - أم البواقي": {
+      "home": 650,
+      "desk": 400
+    },
+    "05 - باتنة": {
+      "home": 700,
+      "desk": 400
+    },
+    "06 - بجاية": {
+      "home": 700,
+      "desk": 400
+    },
+    "07 - بسكرة": {
+      "home": 900,
+      "desk": 500
+    },
+    "08 - بشار": {
+      "home": 1100,
+      "desk": 600
+    },
+    "09 - البليدة": {
+      "home": 500,
+      "desk": 250
+    },
+    "10 - البويرة": {
+      "home": 700,
+      "desk": 400
+    },
+    "11 - تمنراست": {
+      "home": 1300,
+      "desk": 800
+    },
+    "12 - تبسة": {
+      "home": 700,
+      "desk": 400
+    },
+    "13 - تلمسان": {
+      "home": 800,
+      "desk": 400
+    },
+    "14 - تيارت": {
+      "home": 800,
+      "desk": 400
+    },
+    "15 - تيزي وزو": {
+      "home": 700,
+      "desk": 400
+    },
+    "16 - الجزائر": {
+      "home": 500,
+      "desk": 250
+    },
+    "17 - الجلفة": {
+      "home": 900,
+      "desk": 500
+    },
+    "18 - جيجل": {
+      "home": 700,
+      "desk": 400
+    },
+    "19 - سطيف": {
+      "home": 700,
+      "desk": 400
+    },
+    "20 - سعيدة": {
+      "home": 800,
+      "desk": 400
+    },
+    "21 - سكيكدة": {
+      "home": 600,
+      "desk": 400
+    },
+    "22 - سيدي بلعباس": {
+      "home": 700,
+      "desk": 400
+    },
+    "23 - عنابة": {
+      "home": 700,
+      "desk": 400
+    },
+    "24 - قالمة": {
+      "home": 600,
+      "desk": 400
+    },
+    "25 - قسنطينة": {
+      "home": 600,
+      "desk": 400
+    },
+    "26 - المدية": {
+      "home": 700,
+      "desk": 400
+    },
+    "27 - مستغانم": {
+      "home": 700,
+      "desk": 400
+    },
+    "28 - المسيلة": {
+      "home": 800,
+      "desk": 400
+    },
+    "29 - معسكر": {
+      "home": 700,
+      "desk": 400
+    },
+    "30 - ورقلة": {
+      "home": 900,
+      "desk": 500
+    },
+    "31 - وهران": {
+      "home": 800,
+      "desk": 400
+    },
+    "32 - البيض": {
+      "home": 800,
+      "desk": 500
+    },
+    "33 - إليزي": {
+      "home": 1300,
+      "desk": 600
+    },
+    "34 - برج بوعريريج": {
+      "home": 700,
+      "desk": 400
+    },
+    "35 - بومرداس": {
+      "home": 700,
+      "desk": 400
+    },
+    "36 - الطارف": {
+      "home": 700,
+      "desk": 400
+    },
+    "37 - تندوف": {
+      "home": 1300,
+      "desk": 600
+    },
+    "38 - تيسمسيلت": {
+      "home": 800,
+      "desk": 400
+    },
+    "39 - الوادي": {
+      "home": 900,
+      "desk": 500
+    },
+    "40 - خنشلة": {
+      "home": 700,
+      "desk": 500
+    },
+    "41 - سوق أهراس": {
+      "home": 700,
+      "desk": 400
+    },
+    "42 - تيبازة": {
+      "home": 700,
+      "desk": 400
+    },
+    "43 - ميلة": {
+      "home": 700,
+      "desk": 450
+    },
+    "44 - عين الدفلى": {
+      "home": 700,
+      "desk": 400
+    },
+    "45 - النعامة": {
+      "home": 800,
+      "desk": 500
+    },
+    "46 - عين تموشنت": {
+      "home": 800,
+      "desk": 400
+    },
+    "47 - غرداية": {
+      "home": 900,
+      "desk": 500
+    },
+    "48 - غليزان": {
+      "home": 700,
+      "desk": 400
+    },
+    "49 - تيميمون": {
+      "home": 1100,
+      "desk": 600
+    },
+    "50 - برج باجي مختار": {
+      "home": 1200,
+      "desk": 650
+    },
+    "51 - أولاد جلال": {
+      "home": 900,
+      "desk": 500
+    },
+    "52 - بني عباس": {
+      "home": 1100,
+      "desk": 600
+    },
+    "53 - عين صالح": {
+      "home": 1300,
+      "desk": 700
+    },
+    "54 - عين قزام": {
+      "home": 1300,
+      "desk": 700
+    },
+    "55 - توقرت": {
+      "home": 950,
+      "desk": 550
+    },
+    "56 - جانت": {
+      "home": 1100,
+      "desk": 500
+    },
+    "57 - المغير": {
+      "home": 950,
+      "desk": 550
+    },
+    "58 - المنيعة": {
+      "home": 900,
+      "desk": 400
+    }
+  },
+  "FREE_DELIVERY": {
+    "desk": {
+      "enabled": false,
+      "wilayas": [
+        "03 - الأغواط",
+        "04 - أم البواقي",
+        "05 - باتنة",
+        "06 - بجاية",
+        "07 - بسكرة",
+        "08 - بشار",
+        "09 - البليدة",
+        "10 - البويرة",
+        "11 - تمنراست",
+        "12 - تبسة",
+        "13 - تلمسان",
+        "14 - تيارت",
+        "15 - تيزي وزو",
+        "16 - الجزائر",
+        "17 - الجلفة",
+        "18 - جيجل",
+        "19 - سطيف",
+        "20 - سعيدة",
+        "21 - سكيكدة",
+        "22 - سيدي بلعباس",
+        "23 - عنابة",
+        "24 - قالمة",
+        "25 - قسنطينة",
+        "26 - المدية",
+        "27 - مستغانم",
+        "28 - المسيلة",
+        "29 - معسكر",
+        "30 - ورقلة",
+        "31 - وهران",
+        "32 - البيض",
+        "33 - إليزي",
+        "34 - برج بوعريريج",
+        "35 - بومرداس",
+        "36 - الطارف",
+        "37 - تندوف",
+        "38 - تيسمسيلت",
+        "39 - الوادي",
+        "40 - خنشلة",
+        "41 - سوق أهراس",
+        "42 - تيبازة",
+        "43 - ميلة",
+        "44 - عين الدفلى",
+        "45 - النعامة",
+        "46 - عين تموشنت",
+        "47 - غرداية",
+        "48 - غليزان",
+        "49 - تيميمون",
+        "50 - برج باجي مختار",
+        "51 - أولاد جلال",
+        "52 - بني عباس",
+        "53 - عين صالح",
+        "54 - عين قزام",
+        "55 - توقرت",
+        "56 - جانت",
+        "57 - المغير",
+        "58 - المنيعة",
+        "01 - أدرار",
+        "02 - الشلف"
+      ],
+      "minQuantity": 2
+    },
+    "home": {
+      "enabled": false,
+      "wilayas": [
+        "01 - أدرار",
+        "02 - الشلف",
+        "03 - الأغواط",
+        "04 - أم البواقي",
+        "05 - باتنة",
+        "06 - بجاية",
+        "07 - بسكرة",
+        "08 - بشار",
+        "09 - البليدة",
+        "10 - البويرة",
+        "11 - تمنراست",
+        "12 - تبسة",
+        "13 - تلمسان",
+        "14 - تيارت",
+        "15 - تيزي وزو",
+        "16 - الجزائر",
+        "17 - الجلفة",
+        "18 - جيجل",
+        "19 - سطيف",
+        "20 - سعيدة",
+        "21 - سكيكدة",
+        "22 - سيدي بلعباس",
+        "23 - عنابة",
+        "24 - قالمة",
+        "25 - قسنطينة",
+        "26 - المدية",
+        "27 - مستغانم",
+        "28 - المسيلة",
+        "29 - معسكر",
+        "30 - ورقلة",
+        "31 - وهران",
+        "32 - البيض",
+        "33 - إليزي",
+        "34 - برج بوعريريج",
+        "35 - بومرداس",
+        "36 - الطارف",
+        "37 - تندوف",
+        "38 - تيسمسيلت",
+        "39 - الوادي",
+        "40 - خنشلة",
+        "41 - سوق أهراس",
+        "42 - تيبازة",
+        "43 - ميلة",
+        "44 - عين الدفلى",
+        "45 - النعامة",
+        "46 - عين تموشنت",
+        "47 - غرداية",
+        "48 - غليزان",
+        "49 - تيميمون",
+        "50 - برج باجي مختار",
+        "51 - أولاد جلال",
+        "52 - بني عباس",
+        "53 - عين صالح",
+        "54 - عين قزام",
+        "55 - توقرت",
+        "56 - جانت",
+        "57 - المغير",
+        "58 - المنيعة"
+      ],
+      "minQuantity": 2
+    },
+    "freeDeliveryProducts": []
+  },
+  "DISCOUNTS": {
+    "enableQuantityDiscount": true,
+    "minQuantityForDiscount": 1,
+    "discountPerItem": 500,
+    "discountScope": "selected",
+    "discountProducts": [
+      2,
+      3,
+      4
+    ],
+    "enablePromotionalDiscount": false,
+    "promotionalDiscountPercent": 25,
+    "promotionalDiscountedPrice": null,
+    "promoDiscountScope": "all",
+    "promoDiscountProducts": []
+  },
+  "STORE_INFO": {
+    "name": "  معرض الجودة",
+    "storeIcon": "",
+    "storeTitle": " أحدث موديلات ",
+    "tagline": "متجر أفخم الملابس التربية للاطفال",
+    "phoneNumbers": [
+      "0671466489",
+      "0551102155"
+    ],
+    "logo": "https://raw.githubusercontent.com/dz-one1/suift/main/images/store-logo-1764364345578-Screenshot_20251110-233548_Perplexity.jpg",
+    "facebookUrl": "https://www.facebook.com/groups/3131209233768404/?ref=share&mibextid=NSMWBT",
+    "instagramUrl": "",
+    "messengerUrl": "",
+    "viberNumber": "",
+    "whatsappNumber": "213671466489",
+    "whatsappUrl": "https://wa.me/213671466489"
+  },
+  "GOOGLE_SHEETS": {
+    "url": "",
+    "description": "",
+    "autoUpdate": false
+  },
+  "AVAILABLE_COLORS": [
+    "أبيض",
+    "أسود",
+    "أحمر",
+    "أزرق",
+    "أخضر",
+    "وردي",
+    "رمادي",
+    "أصفر"
+  ],
+  "AVAILABLE_SIZES": [
+    "S",
+    "S1",
+    "S2",
+    "S3",
+    "M",
+    "M1",
+    "M2",
+    "M3",
+    "L",
+    "XL",
+    "XXL",
+    "XXXL",
+    "38",
+    "39",
+    "40",
+    "41",
+    "42",
+    "43",
+    "44",
+    "45",
+    "45",
+    "46",
+    "47",
+    "48",
+    "49"
+  ],
+  "SIZE_GUIDE": {
+    "38": {
+      "heightFrom": 150,
+      "heightTo": 160,
+      "weightFrom": 50,
+      "weightTo": 70,
+      "enabled": false
+    },
+    "39": {
+      "heightFrom": 152,
+      "heightTo": 162,
+      "weightFrom": 53,
+      "weightTo": 74,
+      "enabled": false
+    },
+    "40": {
+      "heightFrom": 154,
+      "heightTo": 164,
+      "weightFrom": 56,
+      "weightTo": 78,
+      "enabled": false
+    },
+    "41": {
+      "heightFrom": 156,
+      "heightTo": 166,
+      "weightFrom": 59,
+      "weightTo": 82,
+      "enabled": false
+    },
+    "42": {
+      "heightFrom": 158,
+      "heightTo": 168,
+      "weightFrom": 62,
+      "weightTo": 86,
+      "enabled": false
+    },
+    "43": {
+      "heightFrom": 160,
+      "heightTo": 170,
+      "weightFrom": 65,
+      "weightTo": 90,
+      "enabled": false
+    },
+    "44": {
+      "heightFrom": 162,
+      "heightTo": 172,
+      "weightFrom": 68,
+      "weightTo": 94,
+      "enabled": false
+    },
+    "45": {
+      "heightFrom": 164,
+      "heightTo": 174,
+      "weightFrom": 71,
+      "weightTo": 98,
+      "enabled": false
+    },
+    "46": {
+      "heightFrom": 166,
+      "heightTo": 176,
+      "weightFrom": 74,
+      "weightTo": 102,
+      "enabled": false
+    },
+    "47": {
+      "heightFrom": 168,
+      "heightTo": 178,
+      "weightFrom": 77,
+      "weightTo": 106,
+      "enabled": false
+    },
+    "48": {
+      "heightFrom": 170,
+      "heightTo": 180,
+      "weightFrom": 80,
+      "weightTo": 110,
+      "enabled": false
+    },
+    "49": {
+      "heightFrom": 172,
+      "heightTo": 182,
+      "weightFrom": 83,
+      "weightTo": 114,
+      "enabled": false
+    },
+    "S": {
+      "heightFrom": 120,
+      "heightTo": 130,
+      "weightFrom": 14,
+      "weightTo": 16,
+      "enabled": true
+    },
+    "M": {
+      "heightFrom": 130,
+      "heightTo": 150,
+      "weightFrom": 16,
+      "weightTo": 22,
+      "enabled": true
+    },
+    "L": {
+      "heightFrom": 180,
+      "heightTo": 195,
+      "weightFrom": 70,
+      "weightTo": 90,
+      "enabled": true
+    },
+    "XL": {
+      "heightFrom": 185,
+      "heightTo": 200,
+      "weightFrom": 85,
+      "weightTo": 110,
+      "enabled": true
+    },
+    "XXL": {
+      "heightFrom": 190,
+      "heightTo": 205,
+      "weightFrom": 95,
+      "weightTo": 120,
+      "enabled": false
+    },
+    "S1": {
+      "heightFrom": 160,
+      "heightTo": 180,
+      "weightFrom": 50,
+      "weightTo": 80,
+      "enabled": false
+    },
+    "S2": {
+      "heightFrom": 160,
+      "heightTo": 180,
+      "weightFrom": 50,
+      "weightTo": 80,
+      "enabled": false
+    },
+    "S3": {
+      "heightFrom": 160,
+      "heightTo": 180,
+      "weightFrom": 50,
+      "weightTo": 80,
+      "enabled": false
+    },
+    "M1": {
+      "heightFrom": 160,
+      "heightTo": 180,
+      "weightFrom": 50,
+      "weightTo": 80,
+      "enabled": false
+    },
+    "M2": {
+      "heightFrom": 160,
+      "heightTo": 180,
+      "weightFrom": 50,
+      "weightTo": 80,
+      "enabled": false
+    },
+    "M3": {
+      "heightFrom": 160,
+      "heightTo": 180,
+      "weightFrom": 50,
+      "weightTo": 80,
+      "enabled": false
+    },
+    "XXXL": {
+      "heightFrom": 195,
+      "heightTo": 210,
+      "weightFrom": 105,
+      "weightTo": 140,
+      "enabled": false
+    }
+  }
+};
