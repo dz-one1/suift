eed6393aa228f53e8bb1afb9eb39ce84c3cac67c
dz-one1 <vivavoir150@gmail.com>
2025-12-03 08:16:53 +0100

    تحديث إعدادات المتجر - 03/12/2025 08:16:52

--- config.js
+++ config.js
@@ -526,7 +526,7 @@ const STORE_CONFIG = {
   },
   "DISCOUNTS": {
     "enableQuantityDiscount": true,
-    "minQuantityForDiscount": 1,
+    "minQuantityForDiscount": 2,
     "discountPerItem": 500,
     "discountScope": "all",
     "discountProducts": [],
