1caaa77cd7876bccc7f873b5e59b921f0afbcb18
dz-one1 <vivavoir150@gmail.com>
2025-12-13 06:57:52 +0100

    Update store config file - 13/12/2025 06:57:50

--- config.js
+++ config.js
@@ -584,8 +584,8 @@ const STORE_CONFIG = {
     }
   },
   "POINTS_SYSTEM": {
-    "currentPoints": 500,
-    "remainingDays": 30,
+    "currentPoints": 482,
+    "remainingDays": 29,
     "lastReset": "2025-12-12T00:27:32.315Z",
     "planType": "free",
     "monthlyPoints": 500,
